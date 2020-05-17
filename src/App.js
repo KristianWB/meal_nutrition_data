2aa23e4d6bd5ebecf66ddbbec06a37a6cdca932b
KristianWB <42993999+KristianWB@users.noreply.github.com>
2020-05-17 17:06:25 +0200

    nutrition form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <form>
+          <input>grams</input>
+          <input>kCal</input>
+          <input>Carbohydrates(g)</input>
+          <input>Fat(g)</input>
+          <input>Protein(g)</input>
+          <input>Fiber(g)</input>
+          <input>Price(DKK)</input>
+        </form>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,51 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: 'grams',
+      kiloCalories: 'kCal'
+    };
+
+    this.handleChangeWeight = this.handleChangeWeight.bind(this);
+    this.handleChangeKiloCalories = this.handleChangeKiloCalories.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeWeight(event) {
+        this.setState({weight: event.target.value});        
+  }
+  handleChangeKiloCalories(event) {
+    this.setState({kiloCalories: event.target.value});        
+  }
+  handleSubmit(event) {
+    alert('Data was submitted: ' + this.state.weight + this.state.kiloCalories);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form class='ingridiens_data' onSubmit={this.handleSubmit}>
+        <label>
+          Weight
+          <input type="text" value={this.state.value} onChange={this.handleChangeWeight} />
+        </label>
+        <label>
+          kiloCalories
+          <input type="text" value={this.state.kiloCalories} onChange={this.handleChangeKiloCalories} /> 
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <NameForm />,
+  
   document.getElementById('root')
 );
 
