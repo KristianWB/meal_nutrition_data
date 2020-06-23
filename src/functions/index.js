9e9473e239a9858322c5f93c3cfb5ffe99480d3a
kristianwb <42993999+KristianWB@users.noreply.github.com>
2020-06-23 11:13:32 +0200

    Building stripe

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,11 +1,48 @@
 
 
-exports.handler = async (event, context) => {
+
+require("dotenv").config()
+
+
+// Setting up STRIPE handling constants
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type"
+}
+
+//Setting up STRIPE handling function
+export async function handler(event, context) {
     return {
       statusCode: 200,
       body: JSON.stringify({
         message: "Hi there Tacos",
         event
+      })
+    }
+  }
+
+  if (!event.body || event.httpMethod !== "POST") {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        status: "invalid http method"
+      })
+    }
+  }
+
+  const data = JSON.parse(event.body)
+
+  if (!data.stripeToken || !data.stripeAmt || !data.stripeIdempotency) {
+    console.error("Required information is missing.")
+
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        status: "missing information"
       })
     }
   }
